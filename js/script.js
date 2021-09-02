2dd84f70dbd1b252fbef26e63355971332c193ec
sagorhasanmehedi <mehedihasansagor1995@gmail.com>
2021-09-02 19:43:07 +0600

    final commit

--- js/script.js
+++ js/script.js
@@ -1,10 +1,12 @@
-// onclicka on butyton----->
+// onclicka on sarch button----->
+
 const sarchbutton = async () => {
   const input = document.getElementById("input");
   const inputvalue = input.value;
   input.value = "";
 
-  // inputfild empty condition----->
+  //if inputfild empty (error throw)----->
+
   if (inputvalue === "") {
     document.getElementById("found").style.display = "block";
     document.getElementById("card-parent").textContent = "";
@@ -14,7 +16,7 @@ const sarchbutton = async () => {
     ).innerHTML = `<h3>"Please type something"</h3>`;
   }
 
-  //   url fetch----->
+  //if inputfild  not empty (api fetch)----->
   else {
     const url = ` https://openlibrary.org/search.json?q=${inputvalue}`;
     const res = await fetch(url);
@@ -23,9 +25,9 @@ const sarchbutton = async () => {
   }
 };
 
-// datalode arro function----->
+// datalode function----->
+
 const datalod = (obj) => {
-  // console.log(obj);
   document.getElementById(
     "result-found"
   ).innerHTML = ` <h3>${obj.numFound} result found</h3>`;
@@ -33,31 +35,44 @@ const datalod = (obj) => {
 };
 
 const update = (arr) => {
-  // condition for not found----->
+  // if result not found (error throw)----->
+
   if (arr.length === 0) {
     document.getElementById("found").style.display = "block";
     document.getElementById("card-parent").textContent = "";
     document.getElementById("result-found").style.display = "none";
     document.getElementById("found").innerHTML = `<h3 >No Result Found</h3>`;
-  } else {
+  }
+
+  // if result found (card create and data uplode)----->
+  else {
     document.getElementById("result-found").style.display = "block";
     document.getElementById("found").style.display = "none";
     const parent = document.getElementById("card-parent");
     parent.textContent = "";
-    arr.forEach((book) => {
-      console.log(book.author_name);
 
-      //   card create and uplode----->
+    //   card create and uplode function----->
+    arr.forEach((book) => {
+      console.log(book);
       const newelem = document.createElement("div");
       newelem.classList.add("col");
       newelem.innerHTML = `
             <div class="card h-100 border-0 shadow">
-            <img src="https://covers.openlibrary.org/b/id/${book.cover_i}-L.jpg" class="card-img-top " alt="...">
+            <img src="https://covers.openlibrary.org/b/id/${
+              book.cover_i
+            }-L.jpg" class="card-img-top " alt="...">
             <div class="card-body">
              <h3 class="card-title text-center">${book.title}</h3>
-              <h6 class="card-title text-center "> ${book.author_name}</h6>
+              <h6 class="card-title text-center "> ${
+                book.author_name ? book.author_name : ""
+              }</h6>
+              <h6 class="card-title text-center ">publisher: ${
+                book.publisher?.[0]
+              }</h6>
 
-              <p class="card-text text-center">publish year: ${book.publish_year}</p>
+              <p class="card-text text-center">publish year: ${
+                book.publish_year?.[0]
+              }</p>
             </div>
           </div>
             `;
